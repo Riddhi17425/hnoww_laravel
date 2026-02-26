3d2b93c456e589b3ba3e59dcefec06645ef9c914
htmldesigner7 <htmldesigner7@gmail.com>
2026-02-26 18:30:07 +0530

    Changes

--- resources/views/front/orders/success.blade.php
+++ resources/views/front/orders/success.blade.php
@@ -1,16 +1,16 @@
 @include('layouts.frontheader')
 
- <section class="hero-section_inner">
+<section class="hero-section_inner">
     <img class="img-fluid" src="{{asset('public/images/front/thank-you.webp')}}" alt="him banner">
 
     <div class="hero_content_inner">
-        <h2 class="main_head">Your Order is Confirmed</h2>
-        <p class="text-white py-4">Your request for a bespoke corporate proposal has been successfully received. The ritual of artful
-                    giving begins here. <br>
-                    Our team is reviewing your requirements and will reach out to you shortly at the email address
-                    provided.</p>
-        <a href="{{ route('front.home') }}" class="com_btn border-white text-white"><span><- Back to Home</span></a>
+        <h2 class="main_head">The Ritual Begins.</h2>
+        <h4 class="sub_head text-white">Thank you. Your order has been received.</h4>
+        <p class="text-white py-4">Thank you for choosing to gift with purpose. Your object is now being carefully
+            prepared by our Atelier. <br> We will notify you the moment it is secured and ready for transit.</p>
+        <a href="{{ route('front.home') }}" class="com_btn border-white text-white"><span>
+               <- Return to The Collection</span></a>
     </div>
-</section> 
+</section>
 
 @include('layouts.frontfooter')
