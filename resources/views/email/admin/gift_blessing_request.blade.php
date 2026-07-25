f0d70d19f03f897da938c00924a424682988a340
Riddhi <webdeveloper9.intelliworkz@gmail.com>
2026-07-25 13:33:48 +0530

    Changes

--- resources/views/email/admin/gift_blessing_request.blade.php
+++ resources/views/email/admin/gift_blessing_request.blade.php
@@ -4,13 +4,13 @@
 <tr>
     <td style="padding: 40px;">
         <h1 style="margin: 0 0 10px 0; font-family: 'Times New Roman', Times, serif; font-size: 28px; color: #222222; font-weight: bold;">Hello Admin,</h1>
-        <p style="margin: 0 0 40px 0; font-family: Arial, sans-serif; font-size: 16px; color: #777777;">A new Gift Blesing Request has been received. The details are provided below:</p>
+        <p style="margin: 0 0 40px 0; font-family: Arial, sans-serif; font-size: 16px; color: #777777;">A new Gift Blesing Request has been send. The details are provided below:</p>
 
         <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%"
             style="border-collapse: collapse;">
             <tr>
                 <td width="35%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #888888;">Blessing Title:</td>
-                <td width="65%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #555555;">{{ $blessing_title }}</td>
+                <td width="65%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #555555;">{{ $blessing_title }} ({{ $shareLink }})</td>
             </tr>
             <tr>
                 <td width="35%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #888888;">From Name:</td>
@@ -53,7 +53,7 @@
                 <td width="65%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #555555;">{{ $landmark ?? '' }}</td>
             </tr>
             <tr>
-                <td width="35%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #888888;">Would you like to add beutiful Flowers ?</td>
+                <td width="35%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #888888;">Flowers are Added ?</td>
                 <td width="65%" style="border: 1px solid #dddddd; padding: 15px; font-family: Arial, sans-serif; font-size: 14px; color: #555555;">{{ $add_flowers_label ?? 'No' }}</td>
             </tr>
             @if(!empty($flower_budget_range))
