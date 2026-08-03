021623d567238e26f93d4d5288ec3f52bfbd3f68
Riddhi <webdeveloper9.intelliworkz@gmail.com>
2026-08-03 15:46:37 +0530

    Minor changes

--- resources/views/front/product_details.blade.php
+++ resources/views/front/product_details.blade.php
@@ -105,22 +105,22 @@
                 </div>
             </div> --}}
 
-            {{-- <div class="increment_decrement_area">
+            <div class="increment_decrement_area">
                 <div class="increment_decrement" data-product-id="{{ $product->id }}" data-stock="{{ $product->product_stock }}">
                     <button class="dec_btn" data-call="detail" type="button">−</button>
                     <span class="span_value">1</span>
                     <input type="hidden" class="qty_input" id="product-qty" value="1">
                     <button class="inc_btn" data-call="detail" type="button">+</button>
                 </div>
-                @if($product->product_url == 'the-sovereign-weight' || $product->product_url == 'the-wireless-courtyard')
+                {{--@if($product->product_url == 'the-sovereign-weight' || $product->product_url == 'the-wireless-courtyard')
                     <button type="button" class="com_btn" data-bs-toggle="modal" data-bs-target="#productInquiry">Reserved for June Delivery </button>
-                @else
+                @else --}}
                     <button type="button" class="com_btn add_to_cart_btn" data-product-id="{{ $product->id }}" id="cartSubmitBtn"> Add to Cart</button>
-                @endif
-            </div> --}}
-            <div class="increment_decrement_area">
-                <a href="#" class="com_btn" data-bs-toggle="modal" data-bs-target="#productInquiry">Enquire Now </a>
+                {{-- @endif --}}
             </div>
+            {{-- <div class="increment_decrement_area">
+                <a href="#" class="com_btn" data-bs-toggle="modal" data-bs-target="#productInquiry">Enquire Now </a>
+            </div> --}}
             
             @if(isset($product->large_description) && $product->large_description != '')
             <h4 class="sub_head mb-4">The Story</h4>
