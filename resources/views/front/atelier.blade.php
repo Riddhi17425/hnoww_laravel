41d3154da65a328cc422cb17429ef9bfc1bc5ef9
Riddhi <webdeveloper9.intelliworkz@gmail.com>
2026-08-20 11:54:57 +0530

    client changes

--- resources/views/front/atelier.blade.php
+++ resources/views/front/atelier.blade.php
@@ -47,7 +47,7 @@
             </h2>
             <!-- 3️⃣ Left image (from left) -->
             <div class="text-end magic_wrapper_logo">
-                <img src="{{ asset('public/images/front/home_magic_left.svg') }}" alt="" class="img-fluid">
+                <img src="{{ asset('public/images/front/footer-logo.svg') }}" alt="" class="img-fluid">
             </div>
 
             <!-- 2️⃣ Center image (scale 0 → 1) -->
