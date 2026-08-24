ef32709e860fc366e1b8870b4fd0525786b3ef6f
Agam-IW <webdeveloper11.intelliworkz@gmail.com>
2026-08-24 18:47:32 +0530

    Dynamic category

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,21 @@ use Illuminate\Support\Facades\Artisan;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/send', function () {
+    Mail::raw('This is a test email sent from Laravel.', function ($message) {
+        $message->to('webdeveloper9.intelliworkz@gmail.com')
+            ->subject('Test Mail from Laravel');
+    });
+
+    return 'Mail sent!';
+});
+
+Route::get('/check-mail', function () {
+    return view('email.front.blessing_mail');
+});
 
 Route::get('/clear', function () {
     Artisan::call('optimize:clear');
@@ -71,7 +85,8 @@ Route::name('front.')->group(function () {
     Route::get('/faqs', [FrontController::class, 'getFaqs'])->name('faqs');
     Route::get('/journal', [FrontController::class, 'getJournal'])->name('journal');
     Route::get('/blessings-library/{slug?}', [FrontController::class, 'getBlessings'])->name('blessings.library');
-    Route::get('/blessings-detail/{id?}', [FrontController::class, 'blessingDetailLegacyRedirect'])->name('blessings.detail.legacy');
+    Route::get('/blessings-detail/{id?}', [FrontController::class, 'blessingDetailLegacyRedirect'])
+        ->name('blessings.detail.legacy');
     Route::get('/blessings-audio/{blessing}', [FrontController::class, 'blessingAudio'])->name('blessings.audio');
     Route::post('/gift-blessing', [FrontController::class, 'storeGiftBlessing'])->name('store.gift.blessing');
     Route::post('/shared-details', [FrontController::class, 'storeSharedDetail'])->name('store.shared.detail');
@@ -91,12 +106,12 @@ Route::name('front.')->group(function () {
     Route::get('/bespoke-gifts-dubai', [FrontController::class, 'getBespokeCommission'])->name('bespoke.commission');
     Route::get('/privacy', [FrontController::class, 'getprivacy'])->name('privacy');
 
-    Route::get('/corporate-gifts-dubai/{cat_slug?}', [FrontController::class, 'getCorporateVault'])->name('corporate.vault');
+    Route::get('/luxury-corporate-gifts/{cat_slug?}', [FrontController::class, 'getCorporateVault'])->name('corporate.vault');
     Route::get('/corporate-diwali-collection-2026', [FrontController::class, 'getCorporateDiwaliCollection'])->name('corporate.diwali.collection');
     
     Route::post('/store-festival-inquiry', [FrontController::class, 'storeFestivalInquiry'])->name('store.festival.inquiry');
     Route::get('/get-products-by-category/{category_id}', [FrontController::class, 'getProductsByCategory'])->name('get.products.by.category');
-    Route::get('/wedding-vault', [FrontController::class, 'getWeddingVault'])->name('wedding.vault');
+    Route::get('/wedding-vault', [FrontController::class, 'gextWeddingVault'])->name('wedding.vault');
     Route::post('/wedding-vault/send-email', [FrontController::class, 'sendUnlockWeddingEmail'])->name('wedding-vault.send-email');
     Route::post('/wedding-vault/verify-otp', [FrontController::class, 'verifyWeddingVaultOtp'])->name('wedding-vault.verify-otp');
     Route::get('/wedding-gifts-dubai', [FrontController::class, 'getWeddingVaultInside'])->name('wedding.vault.inside');
@@ -146,10 +161,6 @@ Route::name('front.')->group(function () {
 
     Route::middleware(['auth'])->group(function () {
         Route::get('/profile', [FrontController::class, 'profile'])->name('profile');
-        Route::post('/profile/update', [AuthController::class, 'updateProfile'])->name('profile.update');
-        Route::post('/profile/password', [AuthController::class, 'changePassword'])->name('profile.password');
-        Route::post('/profile/address/{addressId?}', [AuthController::class, 'saveAddress'])->name('profile.address.save');
-        Route::delete('/profile/address/{addressId}', [AuthController::class, 'deleteAddress'])->name('profile.address.delete');
 
         Route::get('/order', [CartController::class, 'order'])->name('order.view');
         Route::get('/order-detail/{orderid}', [CartController::class, 'orderDetail'])->name('order_detail.view');
@@ -265,7 +276,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::put('/update/{blog_id}', [BlogController::class, 'update'])->name('update');
             Route::delete('/delete/{blog_id}', [BlogController::class, 'delete'])->name('delete');
             Route::post('/update-status', [BlogController::class, 'updateStatus'])->name('update.status');
-            Route::post('/send-published-mail/{blog_id}', [BlogController::class, 'sendPublishedMail'])->name('send.published.mail');
         });
 
         Route::resource('journals', JournalController::class);
